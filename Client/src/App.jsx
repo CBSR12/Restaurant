575a4f53e7f49f01fc34a1770000a1c48c1044b1
Shiva Rudra <srudra1024@gmail.com>
2024-12-04 15:39:06 -0700

    Added basic Main Page for navigation

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,23 +1,29 @@
 import { useState } from 'react'
+import MainPage from './Components/MainPage'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [curPage, setCurPage] = useState("main")
 
   return (
     <> 
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+     {curPage === "main" && (
+        <MainPage changeState={(nextPage) => setCurPage(nextPage)}/>
+     )} 
+     {curPage === "customer" && (
+      <div>
+          <p>Customer Page</p>
+          <button onClick={() => setCurPage("main")}>Go back to Main Page</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        
+      
+     )}
+     {curPage === "restaurant" && (
+        <div>
+          <p>Restaurant Page</p>
+          <button onClick={() => setCurPage("main")}>Go back to Main Page</button>
+      </div>
+     )}
     </>
   )
 }
